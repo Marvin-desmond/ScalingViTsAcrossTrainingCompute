bda77961e52fc684f5fc594c0749033a9c015822
Marvin-desmond <marvindesmond@gmail.com>
2025-04-10 01:25:52 -0400

    compute the four steps in the class docstring programmatically, run test on sample single-batch embedding

--- sdpa/__main__.py
+++ sdpa/__main__.py
@@ -1,4 +1,5 @@
 import numpy as np 
+from utils import softmax 
 
 class ScaledDotProductAttention():
     def __init__(self, d_in, d_out, n_heads):
@@ -43,6 +44,7 @@ class ScaledDotProductAttention():
         self.Wout = np.random.randn(d_out, d_out)
         
     def __call__(self, x):
+        import numpy.ma as ma 
         assert len(x.shape) == 3, "batch of embedding sequence expected"
         batch, seq_len, d_in = x.shape
         Q = x @ self.Wq # batch * seq_len * d_out
@@ -56,7 +58,25 @@ class ScaledDotProductAttention():
         Q = Q.transpose((0,2,1,3))
         K = K.transpose((0,2,1,3))
         V = V.transpose((0,2,1,3))
-
+        # compute step 1
+        scores = Q @ K.transpose((0,1,-1,-2))
+        # compute step 2
+        scaled_scores = scores / K.shape[-1]**0.5
+        # compute step 3
+        mask = np.triu(np.ones_like(scores),1)
+        masked_scores = ma.array(scaled_scores, mask=mask).filled(fill_value=-np.inf)
+        weights = softmax(masked_scores)
+        # compute step 4
+        vectors = weights @ V
+        vectors = vectors.reshape((batch,seq_len,self.d_out))
+        context_vectors = vectors @ self.Wout
+        print(context_vectors.shape)        
 
 if __name__ == "__main__":
-    pass
+    d_in = 768
+    d_out = 768
+    n_heads = 12
+    seq_len = 10
+    baseSdpa = ScaledDotProductAttention(d_in, d_out, n_heads)
+    baseSdpa(np.random.randn(1, seq_len, d_in))
+
