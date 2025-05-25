56ad6e32524aefddeb0cd57aa3bc3b4658139de5
Marvin-desmond <marvindesmond@gmail.com>
2025-05-25 13:38:20 -0400

    set tensor device at instantiation, make imports be irrespective of script call directory

--- mha/mha_with_rope.py
+++ mha/mha_with_rope.py
@@ -2,20 +2,26 @@ import torch
 import torch.nn as nn 
 softmax = nn.functional.softmax
 
-from utils import apply_rotary_emb, apply_rotary_v2
-
 import sys 
-sys.path.append("../")
-from posenc_freqs import precompute_freqs_cis
+import os
+import sys
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(current_dir)
+from utils import apply_rotary_emb
+
+mha_root_dir = os.path.abspath(os.path.join(current_dir,".."))
+sys.path.append(mha_root_dir)
+from pos_freqs import precompute_freqs_cis
 
 device = torch.device("cuda" if torch.cuda.is_available() else (
             "mps" if torch.backends.mps.is_available() else "cpu"))
 
 fc = precompute_freqs_cis(64, 32_000, device=device)
-print(fc.unsqueeze(0).unsqueeze(2).shape)
 
 class MHAandRoPE(nn.Module):
-    def __init__(self, d_in, d_out, n_heads, context_window, bias=False):
+    def __init__(self, d_in, d_out, n_heads, context_window, bias=False, device="cpu"):
         super(MHAandRoPE, self).__init__()
         """
         Scaled Dot Product Attention
@@ -52,11 +58,12 @@ class MHAandRoPE(nn.Module):
         self.n_heads = n_heads
         self.d_out = d_out 
         self.head_dim = int(d_out / n_heads) 
-        self.Wq = nn.Linear(d_in, d_out, bias)
-        self.Wk = nn.Linear(d_in, d_out, bias)
-        self.Wv = nn.Linear(d_in, d_out, bias)
-        self.Wout = nn.Linear(d_out, d_out, bias)
-        mask = torch.triu(torch.ones(context_window, context_window), diagonal=1)
+        self.device = device
+        self.Wq = nn.Linear(d_in, d_out, bias, device=device)
+        self.Wk = nn.Linear(d_in, d_out, bias, device=device)
+        self.Wv = nn.Linear(d_in, d_out, bias, device=device)
+        self.Wout = nn.Linear(d_out, d_out, bias, device=device)
+        mask = torch.triu(torch.ones(context_window, context_window,device=device), diagonal=1)
         self.register_buffer("mask", mask)
         
     def __call__(self, x, m_theta_polar_tensor):
@@ -72,7 +79,9 @@ class MHAandRoPE(nn.Module):
         # apply the rope transforms
         # with the m_theta_polar created in the transformer block initializer
         # in the __init__ part of the class
-        Q, K = apply_rotary_emb(Q, K, m_theta_polar_tensor)
+        Q = apply_rotary_emb(Q, m_theta_polar_tensor,self.device)
+        K = apply_rotary_emb(K, m_theta_polar_tensor,self.device)
+      #   Q, K = apply_rotary_emb(Q, K, m_theta_polar_tensor)
         # make each head across the batches have dim (seq_len,head_dim)
         Q = Q.transpose(1,2)
         K = K.transpose(1,2)
@@ -99,8 +108,9 @@ if __name__ == "__main__":
     n_heads = 32 # 8 for original paper, 12 for GPT 2, 32 for Llama2
     seq_len = 100
     context_window = 4096
+   
     m_theta_polar_tensor = precompute_freqs_cis(d_in // n_heads, context_window*2, device=device)
-    baseMHA = MHAandRoPE(d_in, d_out, n_heads, context_window)
+    baseMHA = MHAandRoPE(d_in, d_out, n_heads, context_window, device=device)
     baseMHA = baseMHA.to(device)
 
     input_tensor = torch.rand(batch, seq_len, d_in, device=device) 
