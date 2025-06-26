94f577ccafe093ff4186061fb6f3a9ef0c0ee444
Marvin-desmond <marvindesmond@gmail.com>
2025-06-25 23:16:09 -0400

    move ViT Module to the core_utils file, explicitly state the LayerNorm eps

--- transformerViT/core_utils.py
+++ transformerViT/core_utils.py
@@ -68,14 +68,33 @@ class MLP(nn.Module):
 class ViTBlock(nn.Module):
     def __init__(self,CONFIG):
         super(ViTBlock,self).__init__()
-        self.ln1 = nn.LayerNorm(CONFIG.D_IN)
+        self.ln1 = nn.LayerNorm(CONFIG.D_IN, eps=1e-6)
         self.attn = MultiHeadAttention(CONFIG.D_IN,CONFIG.D_OUT,CONFIG.HEADS)
-        self.ln2 = nn.LayerNorm(CONFIG.D_IN)
+        self.ln2 = nn.LayerNorm(CONFIG.D_IN, eps=1e-6)
         self.mlp = MLP(CONFIG.D_IN,CONFIG.HIDDEN_DIM)
     def forward(self,x):
         x = x + self.attn(self.ln1(x))
         x = x + self.mlp(self.ln2(x))
         return x 
+    
+class VisionTransformer(nn.Module):
+    def __init__(self,CONFIG):
+        super(VisionTransformer,self).__init__()
+        self.image_embeddings = ImageEmbeddings(
+            CONFIG.H, CONFIG.W,
+            CONFIG.P,CONFIG.D_IN
+        )
+        self.vit_blocks = nn.Sequential(
+            *[ViTBlock(CONFIG) 
+              for _ in range(CONFIG.LAYERS)])
+        self.norm = nn.LayerNorm(CONFIG.D_IN, eps=1e-6)
+        self.out_linear = nn.Linear(CONFIG.D_IN,CONFIG.CLASSES)
+    def forward(self,x):
+        x = self.image_embeddings(x)
+        x = self.vit_blocks(x)
+        x = self.norm(x[:, 0])
+        x = self.out_linear(x)
+        return x
     
 def assign(left, right):
     if left.shape != right.shape:
